649f8ae285d998a28eafe58c2bb3219a129e7a1f
Okrekso <semikoseker@gmail.com>
2018-11-14 15:58:41 +0200

    v1_p2

--- web-app/js/modules/constants.js
+++ web-app/js/modules/constants.js
@@ -1,4 +1,4 @@
-export const server = 'http://localhost:3000';
+export const server = 'https://itb-server.herokuapp.com';
 export const s_noAccess = 'access forbidden!';
 export const s_userNotFound= 'user not found!';
 export const s_notTime= 'too many requests, please wait!';
